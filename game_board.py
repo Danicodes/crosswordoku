438102d56b47cca112cdf1abcdd5bd1ed76f38e4
danicodes <danicodes94@gmail.com>
2019-11-01 23:23:23 -0400

    Boilerplate code

---
+++ game_board.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os,sys,re
+import arcade
+
+class Wordoku:
+    def __init__(self, board_x, board_y):
+        self.board = make_board(board_x, board_y)
+        self.score = 0
+
+    def make_board(self, dim_x, dim_y):
+        board = None
+
+        return board
+
+    def draw_board(self, board):
+        drawing = "insert drawing here"
+        print(drawing)
+        return None
